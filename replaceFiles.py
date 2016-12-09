6836c2ee9e3854a26d5d568367076191a8747200
andytower <andytower@users.noreply.github.com>
2016-12-09 21:58:45 +0400

    Init script

---
+++ replaceFiles.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+import shutil
+import sys
+
+def normalizeString(sString):
+    if sString[0:1] == '"':
+        sString = sString[1:len(sString)-1]
+    return sString
+
+def main():
+    if len(sys.argv) == 4:
+        sPath = sys.argv[1]
+        sourceString = sys.argv[2]
+        distString = sys.argv[3]
+    elif len(sys.argv) == 3:
+        sPath = '.'
+        sourceString = sys.argv[1]
+        distString = sys.argv[2]
+    else:
+        print('Incorrect command line argument',len(sys.argv))
+        return
+    files = os.listdir(sPath)
+    nCount = 0
+    for fileName in files:
+        if not os.path.exists(sPath+'/bak'):
+            os.makedirs(sPath+'/bak')
+        if os.path.isfile(os.path.join(sPath,fileName)):
+            shutil.copyfile(os.path.join(sPath,fileName),os.path.join(sPath+'/bak',fileName+'.bak'))
+            descriptorFile = open(os.path.join(sPath,fileName),'r')
+            fileText = descriptorFile.read();
+            descriptorFile.close();
+            descriptorFile = open(os.path.join(sPath,fileName),'w')
+            descriptorFile.write(fileText.replace(sourceString,distString))
+            descriptorFile.close()
+            nCount=nCount+1
+    print('Processing files - ',nCount)
+
+main()
